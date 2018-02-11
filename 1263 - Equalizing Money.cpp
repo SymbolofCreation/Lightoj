e067dffa5d98086e5bf0049cae8099a6aa335117
Protik Nag <protiknag08@gmail.com>
2018-02-11 20:10:30 +0600

    Create 1263 - Equalizing Money.cpp

---
+++ 1263 - Equalizing Money.cpp
@@ -0,0 +1,146 @@
+/*
+ID: protikn2
+PROG: test
+LANG: C++14
+*/
+
+//#include <bits/stdc++.h>
+
+#include<cstdio>
+#include<sstream>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<algorithm>
+#include<set>
+#include<iomanip>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<list>
+#include<iostream>
+#include<fstream>
+#include<numeric>
+#include<string>
+#include<vector>
+#include<cstring>
+#include<map>
+#include<iterator>
+#include<functional>
+#include<climits>
+#include<cassert>
+#include<cstdlib>
+#include<ctime>
+#include<utility>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+//  scan & print
+
+#define         sf              scanf
+#define         pf              printf
+#define         sf1(a)          scanf("%d",&a)
+#define         sf2(a,b)        scanf("%d %d",&a,&b)
+#define         sf3(a,b,c)      scanf("%d %d %d",&a,&b,&c);
+#define         sfl1(a)         scanf("%lld",&a)
+#define         sfl2(a,b)       scanf("%lld %lld",&a,&b)
+#define         sfl3(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+
+#define         pro             printf("PRINT  ::  DEBUG\n")
+#define         pcase           printf("Case %d: ",kase++)
+
+// constants
+
+#define         INF             (1 << 29)
+
+#define         ll              long long int
+#define         pii             pair<int,int>
+
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+
+using namespace std;
+
+int kase=1,n,m,cnt,Sum;
+
+int money[1005];
+bool vis[1005];
+
+vector<int> graph[1005];
+
+inline void reset()
+{
+     for(int i=0; i<1005; i++)
+          graph[i].clear();
+     memset(vis,false,sizeof(vis));
+}
+
+inline void dfs(int node)
+{
+     if(vis[node])
+          return;
+     vis[node] = true;
+     cnt++;
+     Sum += money[node];
+     for(int i=0; i<graph[node].size(); i++)
+     {
+          int x = graph[node][i];
+          dfs(x);
+     }
+}
+
+int main()
+{
+//    freopen (".in", "r", stdin);
+//    freopen (".out", "w", stdout);
+
+     int test;
+     sf1(test);
+
+     while(test--)
+     {
+          reset();
+          sf2(n,m);
+          int sum = 0;
+          for(int i=0; i<n; i++)
+          {
+               cin >> money[i];
+               sum += money[i];
+          }
+
+          for(int i=0; i<m; i++)
+          {
+               int u,v;
+               sf2(u,v);
+               u--,v--;
+               graph[u].push_back(v);
+               graph[v].push_back(u);
+          }
+
+          bool ok = true;
+          int average = sum / n;
+          if(sum%n)
+               ok = false;
+
+          else
+          {
+               for(int i=0; i<n; i++)
+               {
+                    if(vis[i])
+                         continue;
+                    cnt = 0;
+                    Sum = 0;
+                    dfs(i);
+                    if(cnt*average!=Sum)
+                    {
+                         ok=false;
+                         break;
+                    }
+               }
+          }
+
+          pcase;
+          pf("%s\n",ok ? "Yes" : "No");
+     }
+
+     return 0;
+}
