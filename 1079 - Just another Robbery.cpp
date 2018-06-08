f0718322d1388ef8a6bd513fe42babc37ca11e18
Protik Nag <protiknag08@gmail.com>
2018-06-08 13:12:12 +0600

    Create 1079 - Just another Robbery.cpp

---
+++ 1079 - Just another Robbery.cpp
@@ -0,0 +1,112 @@
+/*
+ID: protikn2
+PROG: test
+LANG: C++14
+*/
+
+//    #include <bits/stdc++.h>
+
+#include <cstdio>
+#include <sstream>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <iterator>
+#include <functional>
+#include <climits>
+#include <cassert>
+#include <cstdlib>
+#include <ctime>
+#include <utility>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+//    scan & print
+
+#define         sf              scanf
+#define         pf              printf
+#define         sf1(a)          scanf("%d",&a)
+#define         sf2(a,b)        scanf("%d %d",&a,&b)
+#define         sf3(a,b,c)      scanf("%d %d %d",&a,&b,&c);
+#define         sfl1(a)         scanf("%lld",&a)
+#define         sfl2(a,b)       scanf("%lld %lld",&a,&b)
+#define         sfl3(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+
+#define         pro             printf("PRINT  ::  DEBUG\n")
+#define         pcase           printf("Case %d: ",kase++)
+
+//    constants
+
+#define         INF             (1 << 31)
+
+#define         ll              long long int
+#define         pii             pair<ll,ll>
+
+//	typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+
+using namespace std;
+
+int kase = 1;
+
+int main()
+{
+	freopen ("input.txt", "r", stdin);
+	freopen ("output.txt", "w", stdout);
+
+	int test;
+	cin >> test;
+
+	while(test--) {
+		double p;
+		int n,sum=0;
+
+		std::vector<double> v;
+		std::vector<int> cost;
+
+	 	cin >> p >> n;
+	 	for(int i=0; i<n; i++) {
+ 			int x;
+	 		double y;
+	 		cin >> x >> y;
+	 		cost.push_back(x);
+	 		v.push_back(y);
+	 		sum += x;
+	 	}
+
+	 	double dp[10050];
+	 	for(int i=0; i<10050; i++)
+	 		dp[i] = 5.0;
+	 	dp[0] = 0.0;
+
+	 	for(int i=0; i<n; i++) {
+	 		for(int j=sum; j>=cost[i]	; j--) {
+	 			double temp = dp[j-cost[i]] + (1.0-dp[j-cost[i]]) * v[i];
+	 			if(dp[j]>temp)
+	 				dp[j] = temp;
+	 		}
+	 	}
+
+	 	for(int i=sum; i>=0; i--)
+	 		if(dp[i]<p) {
+	 			pcase;
+	 			cout << i << endl;
+	 			break;
+	 		}
+	}
+
+	return 0;
+}
